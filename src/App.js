b3d416768504a009d16660d114b844135d5a91ac
kevinnail <fumalicious@gmail.com>
2023-01-14 00:59:56 -0800

    added Header.js and Footer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import './App.css';
 import React from 'react';
 import Home from './components/Home/Home.js';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Home />
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return <div className="header">The Sickest Character Builder Ever Built</div>;
+}
